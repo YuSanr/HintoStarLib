b906f8d36dcff512fb499f4eeff80e1621402399
YuSanr <yusanr114514@gmail.com>
2022-11-27 01:00:52 +0800

    版本更新 1.3.0
    大致完善了粒子显示架构

--- src/main/kotlin/cn/HiaXnLib/API/util/ParticleUtil.kt
+++
@@ -1,9 +0,0 @@
-package cn.HiaXnLib.API.util
-
-import net.minecraft.server.v1_8_R3.EnumParticle
-import org.bukkit.Effect
-import org.bukkit.Location
-
-interface ParticleUtil {
-    fun spawnParticle(location: Location, effect: Effect, b: Boolean)
-}
